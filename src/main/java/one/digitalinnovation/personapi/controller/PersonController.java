3d200cedfe55d63cf3fbe0243c971cde8589fff1
Lclocker <lucasmrm18@hotmail.com>
2021-06-25 14:46:03 -0300

    adds algumas coisas

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/ap1/v1/people")
+@RequestMapping("/api/v1/people")
 public class PersonController {
 
     @GetMapping
